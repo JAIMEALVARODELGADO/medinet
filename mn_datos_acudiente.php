0eb66a27c406fc9a20566096b8280408817c5ff5
Unicentro <unicentropastocc@gmail.com>
2024-03-19 17:08:40 -0500

    medinet inicio en git

---
+++ mn_datos_acudiente.php
@@ -0,0 +1,37 @@
+        
+        <fieldset><legend>Información de Acudientes</legend>
+        <div class="fila">
+        <span class="etiqueta"><label for="nombre_acud1">Nombre:</label></span>
+        <span class="form-el"><input type='text' id='nombre_acud1' name='nombre_acud1' size='50' maxlength='50' required=''/></span>
+        </div>
+        <div class="fila">
+        <span class="etiqueta"><label for="telefono_acud1">Teléfono:</label></span>
+        <span class="form-el"><input type='text' id='telefono_acud1' name='telefono_acud1' size='30' maxlength='30' required=''/></span>
+        </div>
+        <div class="fila">
+        <span class="etiqueta"><label for="direccion_acud1">Dirección:</label></span>
+        <span class="form-el"><input type='text' id='direccion_acud1' name='direccion_acud1' size='80' maxlength='80' required=''/></span>
+        </div>
+        <div class="fila">
+        <span class="etiqueta"><label for="parentesco1">Parentesco:</label></span>
+        <span class="form-el"><input type='text' id='parentesco1' name='parentesco1' size='30' maxlength='30' required=''/></span>
+        </div>
+        <hr color='gray' size='1'>
+        <div class="fila">
+        <span class="etiqueta"><label for="nombre_acud2">Nombre:</label></span>
+        <span class="form-el"><input type='text' id='nombre_acud2' name='nombre_acud2' size='50' maxlength='50' required=''/></span>
+        </div>
+        <div class="fila">
+        <span class="etiqueta"><label for="telefono_acud2">Teléfono:</label></span>
+        <span class="form-el"><input type='text' id='telefono_acud2' name='telefono_acud2' size='30' maxlength='30' required=''/></span>
+        </div>
+        <div class="fila">
+        <span class="etiqueta"><label for="direccion_acud2">Dirección:</label></span>
+        <span class="form-el"><input type='text' id='direccion_acud2' name='direccion_acud2' size='80' maxlength='80' required=''/></span>
+        </div>
+        <div class="fila">
+        <span class="etiqueta"><label for="parentesco2">Parentesco:</label></span>
+        <span class="form-el"><input type='text' id='parentesco2' name='parentesco2' size='30' maxlength='30' required=''/></span>
+        </div>
+        </fieldset>
+        
